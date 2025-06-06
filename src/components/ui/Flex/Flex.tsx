cae45a461e44cfa5849c884712f82134633038a0
MSLadanov <MSLadanov@gmail.com>
2025-06-06 21:25:03 +0300

    chore: :construction: creating base ui components

--- src/components/ui/Flex/Flex.tsx
+++ src/components/ui/Flex/Flex.tsx
@@ -1,13 +1,38 @@
 import { ReactElement, ReactNode } from "react";
-import './style.scss'
+import "./style.scss";
 
 interface IFlexProps {
   children: ReactNode;
-  flexDirection: 'column' | 'row';
-  justifyContent: 'space-between' | 'center';
-  alignItems: 'center';
+  flexDirection: "column" | "row";
+  justifyContent:
+    | "space-between"
+    | "center"
+    | "start"
+    | "end"
+    | "flex-start"
+    | "flex-end"
+    | "right"
+    | "left"
+    | "space-around"
+    | "space-evenly";
+  alignItems:
+    | "center"
+    | "stretch"
+    | "start"
+    | "flex-start"
+    | "end"
+    | "flex-end"
+    | "baseline";
 }
 
-export const Flex: React.FC<IFlexProps> = ({ children, flexDirection, alignItems }): ReactElement => {
-  return <div className={`flex-container ${flexDirection} ${alignItems}`}>{children}</div>;
+export const Flex: React.FC<IFlexProps> = ({
+  children,
+  flexDirection,
+  alignItems,
+}): ReactElement => {
+  return (
+    <div className={`flex-container ${flexDirection} ${alignItems}`}>
+      {children}
+    </div>
+  );
 };
