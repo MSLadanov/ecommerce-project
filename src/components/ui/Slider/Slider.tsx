7ca381f1426de52eccac0f48cbd66c3e65b836c7
MSLadanov <MSLadanov@gmail.com>
2025-06-16 18:55:45 +0300

    chore: :construction: creating slider component

--- src/components/ui/Slider/Slider.tsx
+++ src/components/ui/Slider/Slider.tsx
@@ -1,19 +1,30 @@
-import { ReactElement, ReactNode, useState } from "react";
+import { ReactElement, ReactNode, useState, useEffect } from "react";
 import "./style.scss";
 
 interface ISliderProps {
   children: ReactNode;
   withControls?: boolean;
   autoSlide?: boolean;
+  slideDelay?: number;
 }
 
 export const Slider: React.FC<ISliderProps> = ({
   children,
   withControls = true,
   autoSlide = false,
+  slideDelay = 2000,
 }): ReactElement => {
-  const [slides, setSlides] = useState(children)
-  console.log(slides)
+  const [slides, setSlides] = useState(children);
+  const initSlider = () => {
+    console.log("slide");
+  };
+  useEffect(() => {
+    if (autoSlide) {
+      setInterval(() => {
+        initSlider();
+      }, slideDelay);
+    }
+  }, [autoSlide, slideDelay]);
   return (
     <div className="slider">
       <div className="slider__frame"></div>
