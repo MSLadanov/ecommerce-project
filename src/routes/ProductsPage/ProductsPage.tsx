de39a7595f1f9e4e3fb7e725b044b9858057f477
MSLadanov <MSLadanov@gmail.com>
2025-06-17 22:32:23 +0300

    chore: :construction: creating slider component

--- src/routes/ProductsPage/ProductsPage.tsx
+++ src/routes/ProductsPage/ProductsPage.tsx
@@ -3,12 +3,24 @@ import { ProductsList } from "@components/ProductsList";
 import { Slide } from "@components/ui/Slide";
 
 export const ProductsPage = () => {
+  const testSlides = [
+    {
+      id: 1,
+      url: "https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png",
+    },
+    { id: 2, url: "https://en.wikipedia.org/wiki/Image#/media/File:TEIDE.JPG" },
+    {
+      id: 3,
+      url: "https://en.wikipedia.org/wiki/Image#/media/File:Pencil_drawing_of_a_girl_in_ecstasy.jpg",
+    },
+  ];
+  const Slides 
   return (
     <main>
       <Slider>
         <Slide />
-        <Slide/>
-        <Slide/>
+        <Slide />
+        <Slide />
       </Slider>
       <ProductsList />
     </main>
