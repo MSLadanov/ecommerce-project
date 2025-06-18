a98d41aeb4120af39c2836cafa895c09161abe87
MSLadanov <MSLadanov@gmail.com>
2025-06-18 12:06:06 +0300

    chore: :construction: creating slider

--- src/hooks/useSlider.ts
+++ src/hooks/useSlider.ts
@@ -1,15 +1,21 @@
 import { ReactNode, useState } from "react";
 
-export const useSlider = (initialSlides: ReactNode) => {
+export const useSlider = (initialSlides: ReactNode[]) => {
   const [slides, setSlides] = useState(initialSlides);
   const initSlider = () => {
     console.log("slide");
   };
-  const nextSlide = () => {
-    console.log("next");
-  };
   const prevSlide = () => {
-    console.log("prev");
+    const currentSlides = [...slides];
+    const lastSlide = currentSlides.pop();
+    const newSlides = [lastSlide, ...currentSlides];
+    setSlides(newSlides);
+  };
+  const nextSlide = () => {
+    const currentSlides = [...slides];
+    const firstSlide = currentSlides[0];
+    const newSlides = [...currentSlides.slice(1), firstSlide];
+    setSlides(newSlides);
   };
   return { slides, initSlider, nextSlide, prevSlide };
 };
