60f8fd8a915072df88c5b899d9a757e7e09993b6
MSLadanov <MSLadanov@gmail.com>
2025-06-21 12:40:06 +0300

    chore: :construction: creating modal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,9 @@
-import { useModal } from "@/hooks/useModal";
 import { ReactElement } from "react";
-import { AuthModal } from "@components/AuthModal";
 
 export const Header = (): ReactElement => {
-  const [openModal, ModalPortal] = useModal(AuthModal);
   return (
     <div>
       header
-      <button onClick={() => openModal}>Modal</button>
-      {ModalPortal}
     </div>
   );
 };
