519d41eb712a2576d9aba9428ca611df9cd6caba
MSLadanov <MSLadanov@gmail.com>
2025-06-23 12:50:23 +0300

    chore: :construction: creating notify

---
+++ src/hooks/useNotify.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+interface IUseNotifyProps {
+  delay: number;
+}
+
+export const useNotify = ({ delay } : IUseNotifyProps) => {
+  const [isNotifyShowed, setIsNotifyShowed] = useState(false);
+  const toggleNotify = () => {
+    setIsNotifyShowed(true);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      setIsNotifyShowed(false);
+    }, delay);
+  }, [delay, isNotifyShowed]);
+  return { toggleNotify };
+};
