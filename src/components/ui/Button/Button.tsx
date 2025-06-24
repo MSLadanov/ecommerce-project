d33ca7468507d6346db0c554f5f13e4bbbe85985
MSLadanov <MSLadanov@gmail.com>
2025-06-24 20:28:43 +0300

    chore: :construction: creating button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,6 +1,14 @@
-import { ReactElement } from "react";
-import './style.scss'
+import { ReactElement, ReactNode } from "react";
+import "./style.scss";
 
-export const Button = () : ReactElement => {
-    return <button></button>
+interface IButtonProps {
+  children: ReactNode;
+  onClickAction: () => void;
 }
+
+export const Button: React.FC<IButtonProps> = ({
+  children,
+  onClickAction,
+}): ReactElement => {
+  return <button onClick={() => onClickAction()}>{children}</button>;
+};
