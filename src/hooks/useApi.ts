b62dd5e26d3de3145e2e1c96b34824a780d2bd4e
MSLadanov <MSLadanov@gmail.com>
2025-06-25 09:25:39 +0300

    chore: :construction: creating use api hook

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -4,11 +4,17 @@ import axios from "axios";
 
 export const useApi = () => {
   return {
-    async get<T>(requestEndPoint: TRequestEndpoint) {
-      const { data } = await axios.get<T>(API_ENDPOINTS[requestEndPoint]);
+    async get<T>(requestEndpoint: TRequestEndpoint) {
+      const { data } = await axios.get<T>(API_ENDPOINTS[requestEndpoint]);
       return data;
     },
-    async post() {},
+    async post<T>(requestEndpoint: TRequestEndpoint, body) {
+      const { data } = await axios.post<T>(
+        API_ENDPOINTS[requestEndpoint],
+        body
+      );
+      return data;
+    },
     async update() {},
     async delete() {},
   };
