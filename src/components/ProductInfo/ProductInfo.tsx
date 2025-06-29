cb5eceb1ff5ca93d6cfd6d9bc589cf9ab4b06339
MSLadanov <MSLadanov@gmail.com>
2025-06-29 11:19:37 +0300

    chore: :construction: creating product page

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,10 +1,27 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { useSearchParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import { useApi } from "@hooks/useApi";
+import { IProduct } from "@/types/Products";
 import "./style.scss";
 
 export const ProductInfo = (): ReactElement => {
   const [searchParams] = useSearchParams();
   const productId = searchParams.get("id");
-  console.log(productId);
+  const { get } = useApi();
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["product"],
+    queryFn: () => get<IProduct>("PRODUCTS", `/${productId}`),
+  });
+  console.log(data)
+  useEffect(() => {
+    refetch();
+  }, [refetch, searchParams]);
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  if (isError) {
+    return <div>Error</div>;
+  }
   return <div>Product Info</div>;
 };
