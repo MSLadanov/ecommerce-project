73b234f7ad112a54e033350f4897cae648c1d743
MSLadanov <MSLadanov@gmail.com>
2025-07-01 10:03:53 +0300

    chore: :construction: creating discount slider

--- src/components/DiscountSlider/DiscountSlider.tsx
+++ src/components/DiscountSlider/DiscountSlider.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import { Slide } from "../ui/Slide";
 import { Flex } from "../ui/Flex";
 import { FaDollarSign } from "react-icons/fa";
+import { Link } from "react-router";
 import "./style.scss";
 
 export const DiscountSlider = (): ReactElement => {
@@ -29,23 +30,25 @@ export const DiscountSlider = (): ReactElement => {
     <Flex justifyContent="center">
       <Slider autoSlide withControls={false}>
         {data.products.map((product: IProduct) => (
-          <Slide key={product.id} image={product.images[0]}>
-            <div className="discount__info">
-              <h3>{product.brand}</h3>
-              <h4>{product.title}</h4>
-              <Flex className="product-card__price">
-                <h1>
-                  {product.price} <FaDollarSign />
-                </h1>
-                <h2>
-                  {Math.round(
-                    product.price / (1 - product.discountPercentage / 100)
-                  )}{" "}
-                  <FaDollarSign />
-                </h2>
-              </Flex>
-            </div>
-          </Slide>
+          <Link to={`/product?id=${product.id}`}>
+            <Slide key={product.id} image={product.images[0]}>
+              <div className="discount__info">
+                <h3>{product.brand}</h3>
+                <h4>{product.title}</h4>
+                <Flex className="product-card__price">
+                  <h1>
+                    {product.price} <FaDollarSign />
+                  </h1>
+                  <h2>
+                    {Math.round(
+                      product.price / (1 - product.discountPercentage / 100)
+                    )}{" "}
+                    <FaDollarSign />
+                  </h2>
+                </Flex>
+              </div>
+            </Slide>
+          </Link>
         ))}
       </Slider>
     </Flex>
