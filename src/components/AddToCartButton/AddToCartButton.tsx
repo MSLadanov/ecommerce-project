a06e5d907f9d4defba541f416ce41d707c1ee960
MSLadanov <MSLadanov@gmail.com>
2025-07-02 15:12:58 +0300

    chore: :construction: creating add to cart button

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -1,6 +1,9 @@
 import { ReactElement, ReactNode } from "react";
 import { IProduct } from "@/types/Products";
 import { useCart, ICartState } from "@hooks/useCart";
+import { Flex } from "@components/ui/Flex";
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
 import "./style.scss";
 
 interface IAddToCartButtonProps {
@@ -8,10 +11,29 @@ interface IAddToCartButtonProps {
   productData: IProduct;
 }
 
+const InCartControls = (): ReactElement => {
+  return (
+    <Flex justifyContent="space-between" className="in-cart-controls">
+      <Flex justifyContent="center" alignItems="align-center">
+        <FaPlus />
+      </Flex>
+      <Flex justifyContent="center" alignItems="align-center">
+        0
+      </Flex>
+      <Flex justifyContent="center" alignItems="align-center">
+        <FaMinus />
+      </Flex>
+    </Flex>
+  );
+};
+
 export const AddToCartButton: React.FC<IAddToCartButtonProps> = ({
   children,
   productData,
 }): ReactElement => {
   const { cart } = useCart((state: ICartState) => state);
+  if (cart.find((item) => item.id === productData.id)) {
+    return <InCartControls />;
+  }
   return <div onClick={() => console.log(productData, cart)}>{children}</div>;
 };
