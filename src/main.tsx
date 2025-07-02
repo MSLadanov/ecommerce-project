afc001bf9201d0d0e2f50a6907f2906905835e01
MSLadanov <MSLadanov@gmail.com>
2025-07-02 14:07:27 +0300

    chore: :arrow_up: add zustand, remove redux

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import { ProductPage } from "@routes/ProductPage";
 import { ErrorPage } from "@routes/ErrorPage";
 import { UserPage } from "@routes/UserPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
 import { ProductsPage } from "@routes/ProductsPage";
 import "./index.css";
 import "./globals.scss";
@@ -17,7 +15,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />}>
@@ -29,7 +26,6 @@ createRoot(document.getElementById("root")!).render(
             <Route path="*" element={<ErrorPage />} />
           </Routes>
         </BrowserRouter>
-      </Provider>
     </QueryClientProvider>
   </StrictMode>
 );
