0aad544a1564560c72266a7a94dcd10b4f565100
MSLadanov <MSLadanov@gmail.com>
2025-07-04 11:35:35 +0300

    chore: :construction: creating cart page

---
+++ src/components/AllUserCarts/AllUserCarts.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import './style.scss'
+
+export const AllUserCarts = (): ReactElement => {
+  return <div>All user carts</div>;
+};
--- src/routes/CartPage/CartPage.tsx
+++ src/routes/CartPage/CartPage.tsx
@@ -1,3 +1,4 @@
+import { AllUserCarts } from "@components/AllUserCarts";
 import { CurrentCart } from "@components/CurrentCart";
 import { ReactElement } from "react";
 
@@ -5,6 +6,7 @@ export const CartPage = (): ReactElement => {
   return (
     <main>
       <CurrentCart />
+      <AllUserCarts />
     </main>
   );
 };
