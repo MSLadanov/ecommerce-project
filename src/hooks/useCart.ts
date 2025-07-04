aaf3b2e17b4b912dd57d8d2ac0645e8509e6a8e9
MSLadanov <MSLadanov@gmail.com>
2025-07-04 10:53:42 +0300

    chore: :construction: make cart persist

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,5 +1,6 @@
 import { IProduct } from "@/types/Products";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export interface ICartState {
   cart: IProduct[];
@@ -7,16 +8,24 @@ export interface ICartState {
   removeFromCart: (product: IProduct) => void;
 }
 
-export const useCart = create<ICartState>((set) => ({
-  cart: [],
-  addToCart: (product: IProduct) =>
-    set((state: ICartState) => ({ cart: [...state.cart, product] })),
-  removeFromCart: (product: IProduct) =>
-    set((state: ICartState) => {
-      const index = state.cart.findIndex((item) => item.id === product.id);
-      if (index === -1) return { cart: state.cart };
-      return {
-        cart: [...state.cart.slice(0, index), ...state.cart.slice(index + 1)],
-      };
+export const useCart = create<ICartState>()(
+  persist(
+    (set) => ({
+      cart: [],
+      addToCart: (product: IProduct) =>
+        set((state: ICartState) => ({ cart: [...state.cart, product] })),
+      removeFromCart: (product: IProduct) =>
+        set((state: ICartState) => {
+          const index = state.cart.findIndex((item) => item.id === product.id);
+          if (index === -1) return { cart: state.cart };
+          return {
+            cart: [
+              ...state.cart.slice(0, index),
+              ...state.cart.slice(index + 1),
+            ],
+          };
+        }),
     }),
-}));
+    { name: "cart-storage" }
+  )
+);
