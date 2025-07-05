4b9063a73e9fa03ce02e2c51c3c7d3e01ce2eb9b
MSLadanov <MSLadanov@gmail.com>
2025-07-05 14:10:38 +0300

    chore: :construction: creating loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,8 +3,10 @@ import "./style.scss";
 
 export const Loader = (): ReactElement => {
   return (
-    <div className="loader">
+    <div className="loader-layout">
+      <div className="loader">
       <p>W-BOZONE</p>
+    </div>
     </div>
   );
 };
