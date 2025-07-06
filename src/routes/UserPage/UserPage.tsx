682548bdbe4137bdf9777c82fc434bdcab786ec0
MSLadanov <MSLadanov@gmail.com>
2025-07-06 09:33:10 +0300

    chore: :construction: creating user page

--- src/routes/UserPage/UserPage.tsx
+++ src/routes/UserPage/UserPage.tsx
@@ -1,9 +1,11 @@
+import { UserInfo } from "@components/UserInfo/UserInfo";
 import { AllUserCarts } from "@components/AllUserCarts";
 import { ReactElement } from "react";
 
 export const UserPage = () : ReactElement => {
-  return <main>
- <AllUserCarts />
-
-  </main>;
+  return (
+    <main>
+      <UserInfo/>
+      <AllUserCarts />
+    </main>);
 };
