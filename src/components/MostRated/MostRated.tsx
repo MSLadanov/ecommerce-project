f498b49e20a8d19e81616a70369da2b25a097b9d
MSLadanov <MSLadanov@gmail.com>
2025-07-07 12:16:40 +0300

    chore: :construction: creating most rated component

--- src/components/MostRated/MostRated.tsx
+++ src/components/MostRated/MostRated.tsx
@@ -4,7 +4,42 @@ import { useApi } from "@hooks/useApi";
 import { useQuery } from "@tanstack/react-query";
 import { IProductsResponse } from "@/types/Products";
 import { Loader } from "@components/Loader";
+import { FaDollarSign } from "react-icons/fa";
+import { IProduct } from "@/types/Products";
+import { Flex } from "@components/ui/Flex";
 import "./style.scss";
+
+const MostRatedCard: React.FC<Partial<IProduct>> = ({
+  id,
+  images,
+  brand,
+  title,
+  price,
+  discountPercentage,
+}): ReactElement => {
+  return (
+    <Flex className="most-rated-card" flexDirection="row" alignItems="align-center">
+      <a href={`/product?id=${id}`}>
+        <Flex className="most-rated-card__image" alignItems="align-center">
+          <img src={images[0]} alt={title + " image"} />
+        </Flex>
+      </a>
+      <div className="most-rated-card__info">
+        <h3>{brand}</h3>
+        <h4>{title}</h4>
+        <Flex className="most-rated-card__price">
+          <h1>
+            {price} <FaDollarSign />
+          </h1>
+          <h2>
+            {Math.round(price / (1 - discountPercentage / 100))}{" "}
+            <FaDollarSign />
+          </h2>
+        </Flex>
+      </div>
+    </Flex>
+  );
+};
 
 export const MostRated = (): ReactElement => {
   const { get } = useApi();
@@ -22,7 +57,14 @@ export const MostRated = (): ReactElement => {
   return (
     <Grid size="xs" className="most-rated">
       {data.products.map((product) => (
-        <div>{product.title}</div>
+        <MostRatedCard
+          key={product.id}
+          id={product.id}
+          images={product.images}
+          title={product.title}
+          price={product.price}
+          discountPercentage={product.discountPercentage}
+        />
       ))}
     </Grid>
   );
