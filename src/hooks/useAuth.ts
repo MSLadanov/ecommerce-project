f827a3448b42a2a5ee1b14a3c2c80e47398cafb6
MSLadanov <MSLadanov@gmail.com>
2025-07-07 10:14:09 +0300

    chore: :construction: creating useauth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,17 +3,22 @@ import { useApi } from "./useApi";
 import { useEffect, useState } from "react";
 import { IUser } from "@/types/Users";
 
-
 export const useAuth = () => {
-  const [cookie] = useCookies(["authToken"]);
+  const [cookie, , removeCookie] = useCookies(["authToken"]);
   const { get } = useApi();
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
   const [userData, setUserData] = useState<IUser>(null);
+  const clearAuth = () => {
+    if (cookie) {
+      removeCookie("authToken");
+    }
+  };
   const checkUserAuth = async () => {
     setIsLoading(true);
     try {
-      const user : IUser = await get("AUTH_ME", "", {
+      const user: IUser = await get("AUTH_ME", "", {
         method: "GET",
         headers: {
           Authorization: `Bearer ${cookie.authToken}`,
@@ -21,6 +26,7 @@ export const useAuth = () => {
       });
       setIsLoading(false);
       setUserData(user);
+      setIsAuth(true);
     } catch (error) {
       setIsError(error.response.data.message);
     } finally {
@@ -31,5 +37,5 @@ export const useAuth = () => {
     checkUserAuth();
   }, []);
 
-  return { userData, isLoading, isError };
+  return { userData, isLoading, isError, clearAuth, isAuth };
 };
