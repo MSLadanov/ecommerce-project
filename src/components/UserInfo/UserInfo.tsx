154e383117b0c2647d43a4e896bec2081e128ce8
MSLadanov <MSLadanov@gmail.com>
2025-07-07 09:52:49 +0300

    chore: :construction: creating user page

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -12,5 +12,18 @@ export const UserInfo = (): ReactElement => {
   if (isError) {
     return <div>Error</div>;
   }
-  return <Flex className="user-info">{userData.address.address}</Flex>;
+  return (
+    <Flex className="user-info" flexDirection="row" justifyContent="center">
+      <Flex className="user-info__image">
+        <img src={userData?.image} alt={userData?.firstName + " avatar"} />
+      </Flex>
+      <Flex className="user-info__bio" flexDirection="column">
+        <Flex>{userData?.firstName}</Flex>
+        <Flex>{userData?.maidenName}</Flex>
+        <Flex>{userData?.lastName}</Flex>
+        <Flex>{userData?.email}</Flex>
+        <Flex>{userData?.phone}</Flex>
+      </Flex>
+    </Flex>
+  );
 };
