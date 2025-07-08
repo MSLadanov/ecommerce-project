835b76b1c1e5349c946bd08442e8cea01c8c7c41
MSLadanov <MSLadanov@gmail.com>
2025-07-08 13:23:18 +0300

    chore: :construction: creating search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,25 @@
 import { Input } from "@components/ui/Input";
 import { ReactElement } from "react";
 import { useSearch } from "@hooks/useSearch";
+import { Flex } from "@components/ui/Flex";
 import "./style.scss";
+
+const SearchResults = (): ReactElement => {
+  return <div className="search-results">sdfsd</div>;
+};
 
 export const Search = (): ReactElement => {
   const { search, setSearch } = useSearch((state) => state);
-  return <Input className="search-bar" id="search" type="text" value={search} setValue={setSearch} />;
+  return (
+    <Flex className="search-box" flexDirection="column">
+      <Input
+        className="search-bar"
+        id="search"
+        type="text"
+        value={search}
+        setValue={setSearch}
+      />
+      <SearchResults />
+    </Flex>
+  );
 };
