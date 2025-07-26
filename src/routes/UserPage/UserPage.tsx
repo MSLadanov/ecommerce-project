88ad9a8ded6922aceb38e00383e812cc6b1ba21a
MSLadanov <MSLadanov@gmail.com>
2025-07-26 11:25:44 +0300

    chore: :construction: creating wishlist

--- src/routes/UserPage/UserPage.tsx
+++ src/routes/UserPage/UserPage.tsx
@@ -2,8 +2,12 @@ import { UserInfo } from "@components/UserInfo/UserInfo";
 import { AllUserCarts } from "@components/AllUserCarts";
 import { ReactElement } from "react";
 import { CollapseBox } from "@components/ui/CollapseBox";
+import { CartItem } from "@components/CartItem";
+import { useFavourites } from "@hooks/useFavourites";
+import { IProduct } from "@/types/Products";
 
 export const UserPage = (): ReactElement => {
+  const { favourites } = useFavourites((state) => state);
   return (
     <main>
       <UserInfo />
@@ -11,7 +15,11 @@ export const UserPage = (): ReactElement => {
       <CollapseBox title="Addresses">Addresses</CollapseBox>
       <CollapseBox title="Payment Methods">Payment Methods</CollapseBox>
       <CollapseBox title="Returns & Refunds">Returns & Refunds</CollapseBox>
-      <CollapseBox title="Wishlist">Wishlist</CollapseBox>
+      <CollapseBox title="Wishlist">
+        {favourites.map((item: IProduct) => (
+          <CartItem product={item} isCurrentCart={false} />
+        ))}
+      </CollapseBox>
       <CollapseBox title="Recently Viewed">Recently Viewed</CollapseBox>
     </main>
   );
