73d093061b3233649f195314f3f3ca80437aef5b
MSLadanov <MSLadanov@gmail.com>
2025-07-27 17:42:39 +0300

    chore: :construction: creating loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,12 +1,10 @@
 import { ReactElement } from "react";
-import "./style.scss";
+import './style.scss'
 
 export const Loader = (): ReactElement => {
   return (
-    <div className="loader-layout">
-      <div className="loader">
-      <p>W-BOZONE</p>
-    </div>
+    <div className="circle-loader">
+      <div className="circle-loader__spinner"></div>
     </div>
   );
 };
