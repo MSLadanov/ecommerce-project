f322c19a436a69d706c3b46e907dce4e4cfd218b
MSLadanov <MSLadanov@gmail.com>
2025-07-27 18:28:05 +0300

    fix: :bug: fix screen scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Header } from "@components/Header";
 import { Footer } from "./components/Footer";
 import { Outlet } from "react-router";
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router";
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
   return (
     <div>
       <Header />
