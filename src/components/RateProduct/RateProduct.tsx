f170ed6b25dfdc78f0e577bb2b69df5453d39e9f
MSLadanov <MSLadanov@gmail.com>
2025-07-29 11:29:52 +0300

    chore: :construction: creating rate product

--- src/components/RateProduct/RateProduct.tsx
+++ src/components/RateProduct/RateProduct.tsx
@@ -3,15 +3,19 @@ import { Flex } from "@components/ui/Flex";
 import { TextArea } from "@components/ui/TextArea";
 import { Button } from "@components/ui/Button";
 import { FaCommentAlt } from "react-icons/fa";
+import { StarsInput } from "@components/StarsInput";
 import "./style.scss";
 
 export const RateProduct = (): ReactElement => {
   return (
-    <Flex className="rate-product" justifyContent="space-between">
-      <TextArea id="rate-field" name="rate-field" />
-      <Button styleGuide="ozon">
-        <FaCommentAlt />
-      </Button>
+    <Flex className="rate-product__wrapper" flexDirection="column">
+      <StarsInput/>
+      <Flex className="rate-product" justifyContent="space-between">
+        <TextArea id="rate-field" name="rate-field" />
+        <Button styleGuide="ozon">
+          <FaCommentAlt />
+        </Button>
+      </Flex>
     </Flex>
   );
 };
---
+++ src/components/StarsInput/StarsInput.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import "./style.scss";
+
+export const StarsInput = (): ReactElement => {
+  return <div className="stars-input">Stars input</div>;
+};
