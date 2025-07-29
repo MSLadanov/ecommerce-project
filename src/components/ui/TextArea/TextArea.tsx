962880cfd53b591e311c3054ca6eaa6a2d8a5b23
MSLadanov <MSLadanov@gmail.com>
2025-07-29 10:53:02 +0300

    chore: :construction: creating text area and rate product

--- src/components/ui/TextArea/TextArea.tsx
+++ src/components/ui/TextArea/TextArea.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react"
 
 export const TextArea = () : ReactElement => {
-    return <div>
+    return <div className="text-field">
         <label htmlFor=""></label>
         <textarea name="" id=""></textarea>
     </div>
