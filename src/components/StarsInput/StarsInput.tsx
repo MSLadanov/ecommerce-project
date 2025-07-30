dbf8f80a79f37fe19ec7a82e331eb2e5598e0666
MSLadanov <MSLadanov@gmail.com>
2025-07-30 19:04:33 +0300

    chore: :construction: creating rate product

--- src/components/StarsInput/StarsInput.tsx
+++ src/components/StarsInput/StarsInput.tsx
@@ -2,34 +2,43 @@ import { ReactElement, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import "./style.scss";
 
-const Star = ({
-  value,
-  rate,
-  setRate,
-}: {
-  value: number;
-  rate: number;
-  setRate: (newRate: number) => void;
-}): ReactElement => {
+interface IStarProps {
+  value: number
+  onClick: () => void
+  onFocus: () => void
+  onBlur: () => void
+}
+
+const Star : React.FC<IStarProps> = ({value, onClick, onFocus, onBlur}): ReactElement => {
   return (
     <FaStar
-      onClick={() => setRate(value)}
-      color={rate <= value && rate !== 0 ? "yellow" : "black"}
+      onClick={onClick}
+      onFocus={onFocus}
+      onBlur={onBlur}
     />
   );
 };
 
 export const StarsInput = (): ReactElement => {
   const [currentRate, setCurrentRate] = useState(0);
+  const [currentFocus, setCurrentFocus] = useState(0)
   const starValues = [1, 2, 3, 4, 5];
+  const handleStarClick = (value: number) => {
+    console.log(value)
+  }
+  const handleStarFocus = (value: number) => {
+    setCurrentFocus(value)
+  }
+  const handleStarBlur = () => {
+    setCurrentFocus(0)
+  }
   return (
     <div className="stars-input">
       {starValues.map((value, index) => (
         <Star
-          value={value}
-          key={index}
-          rate={currentRate}
-          setRate={() => setCurrentRate}
+          onClick={() => handleStarClick(value)}
+          onFocus={() => handleStarFocus(value)}
+          onBlur={() => handleStarBlur()}
         />
       ))}
     </div>
